ec2e55d0bc3e1aaf4f73bb8377d998c0767ba917
DavidVillaseca <43218116+DavidVillaseca@users.noreply.github.com>
2022-11-08 23:37:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.image(
 
 st.header("Opacidad pulmonar y su mascara")
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+uploaded_files = st.file_uploader("Subir una imagen", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
